80ed60231fec29f21fe1dab83ba20b4337e208e3
Ma3querader <593661332@qq.com>
2019-11-15 17:49:12 +0800

    异常中

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,13 @@
 public class Test {
 
+    public static void main(String[] args) {
+        int round = (int) Math.round(Math.random());
+        try {
+            System.out.println(round);
+        } catch (ArithmeticException e) {
+
+        }
+    }
 
 }
 
