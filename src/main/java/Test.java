ce015e59ad58bd817b6fe20840394b47bad9adce
Ma3querader <593661332@qq.com>
2019-12-20 15:51:44 +0800

    1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,13 +1,11 @@
+
 public class Test {
 
     public static void main(String[] args) {
-        int round = (int) Math.round(Math.random());
-        try {
-            System.out.println(round);
-        } catch (ArithmeticException e) {
 
-        }
     }
+
+
 
 }
 
